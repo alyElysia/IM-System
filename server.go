7490b442ae506af1ea09e3ca9c58ee32736f11f0
alyElysia <3171542784@qq.com>
2024-01-29 17:02:12 +0800

    第三次提交：将用户发送的消息进行广播

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -65,6 +66,29 @@ func (this *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线的消息
 	this.BroadCast(user, "already online~")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "already outline ~")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err: ", err)
+				return
+			}
+
+			// 提取用户的消息（需要去除'\n'）
+			msg := string(buf[:n-1])
+
+			// 将得到的消息进行广播
+			this.BroadCast(user, msg)
+		}
+	}()
 
 	//当前handler阻塞
 	select {}
